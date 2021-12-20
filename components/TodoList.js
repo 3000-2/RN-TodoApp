0c24c7e81fb237a9a5294cdb8697f36afcbca91e
3002 <psch0493@gmail.com>
2021-12-20 23:58:02 +0900

    add component TodoItem

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, View} from 'react-native';
+import TodoItem from './TodoItem';
 
 const TodoList = ({todos}) => {
   return (
     <FlatList
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
       style={styles.list}
       data={todos}
       renderItem={({item}) => (
-        <View>
-          <Text>{item.text}</Text>
-        </View>
+        <TodoItem id={item.id} text={item.text} done={item.done} />
       )}
       keyExtractor={item => item.id.toString()}
     />
@@ -19,6 +19,10 @@ const TodoList = ({todos}) => {
 const styles = StyleSheet.create({
   list: {
     flex: 1,
+  },
+  separator: {
+    backgroundColor: '#e0e0e0',
+    height: 1,
   },
 });
 
